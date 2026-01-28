0440a9e8212dee326c9ea0772e5fd2ea408bfc89
Shugaei <sugaevaanel@gmail.com>
2026-01-28 17:39:42 +0500

    new functions

--- taskmanager/src/main/java/com/taskmanager/service/AuthService.java
+++ taskmanager/src/main/java/com/taskmanager/service/AuthService.java
@@ -1,48 +1,8 @@
-// package com.taskmanager.service;
-
-// import java.util.HashMap;
-// import java.util.Map;
-
-// import com.taskmanager.model.User;
-
-// public class AuthService {
-
-//     private Map<String, User> users = new HashMap<>();
-
-//     // Регистрация нового пользователя
-//     public User register(String username, String password) {
-//         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
-//             throw new IllegalArgumentException("Имя пользователя и пароль не могут быть пустыми");
-//         }
-//         if (users.containsKey(username)) {
-//             throw new IllegalArgumentException("Пользователь уже существует");
-//         }
-//         User user = new User(username, password);
-//         users.put(username, user);
-//         return user;
-//     }
-
-//     // Авторизация пользователя
-//     public boolean login(String username, String password) {
-//         User user = users.get(username);
-//         if (user == null) return false;
-//         return user.getPassword().equals(password);
-//     }
-
-//     // Проверка, существует ли пользователь
-//     public boolean userExists(String username) {
-//         return users.containsKey(username);
-//     }
-// }
-
-
 package com.taskmanager.service;
-
 import java.util.HashMap;
 import java.util.Map;
-
-import com.taskmanager.model.User;
 import org.mindrot.jbcrypt.BCrypt;
+import com.taskmanager.model.User;
 
 public class AuthService {
 
